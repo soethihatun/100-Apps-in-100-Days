2ac4c5a3d7f309c366929e4ccbb16dcdbe3fa7b6
soethiha <ygn.soethiha94@gmail.com>
2014-02-10 06:19:13 +0630

    first commit

---
+++ Day-100/hello.c
@@ -0,0 +1,66 @@
+/*************************************************
+ 
+ Day 100
+ "Hello, world!"
+ Soe Thiha Tun
+ 
+ Ques : Write a program that prints the following message," Hello, world!".
+ 
+*************************************************/
+
+/*
+============== Pseudo ==============
+   1. Start
+   2. Print out the message, "Hello, world!"
+   3. End
+   
+=============== code ===============
+*/
+
+/*-----------------------------------------------
+    Asking for some built-in codes for Input & Output
+    Header File : Standard Input/Output
+------------------------------------------------*/
+
+#include<stdio.h>       // This is preprocessor directive
+
+/*-----------------------------------------------
+    Crete the main function for this particular program
+    Function =>
+    Name : main
+    Type : int(Integer)
+------------------------------------------------*/
+int main(void)
+{
+    // Let's print out the message
+    printf("Hello, world! \n");
+    
+    /*
+    
+        printf = the funcion from "stdio.h"
+        () = is parentheses between which are arguments for that printf function
+        "" = is quotes between which is string objects
+        \n = is special sign that says the compiler to create a new line
+    
+    */
+    
+    
+}
+
+
+// Codes without comments
+/*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+#include<stdio.h>
+
+int main(void)
+{
+    printf("Hello, world! \n");
+}
+
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/
+
+
+
+
+
